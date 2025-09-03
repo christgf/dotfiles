b4979e983990bf19ca86e5e83d0f7259f6cfc157
George Christou <christgf@gmail.com>
2025-09-03 13:37:14 +0300

    zsh

---
+++ zsh/.zsh/exports.zsh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+#
+# Environment variables, plugin and script overrides, paths, and other exports.
+
+# Private go repositories and proxy configuration for private organizations.
+#export GOPRIVATE="github.com/privately,buf.build/gen/go"
+
+# Environment variables for voodoo-nakama.
+#
+
